e81386a71466eaceee12995f768e0143df0f744b
Guilherme Garcia <guilherme4garcia@gmail.com>
2021-09-30 18:17:32 -0300

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server')
+const userSchema = require('./user/schema/user.graphql')
+const userResolvers = require('./user/resolvers/userResolvers')
+const UsersAPI = require('./user/datasource/user')
+
+
+
+
+/// SDL, linguagem de definiçao de schema do GraphQL 
+
+
+const typeDefs = [userSchema]
+const resolvers = [userResolvers]
+
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            usersAPI: new UsersAPI()
+        }
+    } 
+})
+
+server.listen().then(({url}) => {
+    console.log(`Servidor rodando na porta ${url}`)
+})
